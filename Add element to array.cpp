c544ce629b4b787982cb172247bbc68092a4a8ba
Akhilshiva2k4 <132322923+Akhilshiva2k4@users.noreply.github.com>
2024-08-02 14:30:56 +0530

    Add files via upload

---
+++ Add element to array.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int size;
+	printf("Enter size of array:\n");
+	scanf("%d",&size);
+	int arr[100];
+	printf("Enter array elements:\n");
+	for(int i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("Enter element to add:\n");
+	int n,pos;
+	scanf("%d",&n);
+	printf("Enter position:\n");
+	scanf("%d",&pos);
+	pos-=1;
+	for(int i=pos+1;i<size+2;i++)
+	{
+		arr[i]=arr[i+1];
+	}
+	printf("Array after adding element:\n");
+	for(int i=0;i<size+1;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
